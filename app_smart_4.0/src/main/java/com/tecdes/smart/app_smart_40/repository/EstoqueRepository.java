b5aad6c41509c6261784305a591d7623d6654e1d
Leonardo Da SIlva <leonardo_silva48@estudante.sesisenai.org.br>
2026-05-14 21:13:09 -0300

    EstoqueRepository
    Calculo de estoque e filtro de disponibilidade por cor.

--- app_smart_4.0/src/main/java/com/tecdes/smart/app_smart_40/repository/EstoqueRepository.java
+++ app_smart_4.0/src/main/java/com/tecdes/smart/app_smart_40/repository/EstoqueRepository.java
@@ -4,6 +4,8 @@ import com.tecdes.smart.app_smart_40.model.Estoque;
 import com.tecdes.smart.app_smart_40.model.enums.CorBloco;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -15,4 +17,9 @@ public interface EstoqueRepository extends JpaRepository<Estoque, Long> {
     Optional<Estoque> findByPosicao(Integer posicao);
 
     List<Estoque> findByCorBloco(CorBloco corBloco);
+
+    List<Estoque> findByCorBlocoAndQuantidadeGreaterThan(CorBloco corBloco, Integer quantidade);
+
+    @Query("SELECT SUM(e.quantidade) FROM Estoque e WHERE e.corBloco = :corBloco")
+    Integer sumQuantidadeByCorBloco(@Param("corBloco") CorBloco corBloco);
 }
