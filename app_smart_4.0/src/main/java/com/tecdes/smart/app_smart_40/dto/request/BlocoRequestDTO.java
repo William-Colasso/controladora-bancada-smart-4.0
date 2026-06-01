e78b45476cba80a16cabc126d16a5ff655d88182
William-Colasso <william.colasso@gmail.com>
2026-05-31 21:01:45 -0300

    refac: convertendo para versão semantica de cada entidade DTO

---
+++ app_smart_4.0/src/main/java/com/tecdes/smart/app_smart_40/dto/request/BlocoRequestDTO.java
@@ -0,0 +1,26 @@
+package com.tecdes.smart.app_smart_40.dto.request;
+
+import java.util.List;
+
+import com.tecdes.smart.app_smart_40.model.Bloco;
+import com.tecdes.smart.app_smart_40.model.enums.AndarBloco;
+import com.tecdes.smart.app_smart_40.model.enums.CorBloco;
+
+public record BlocoRequestDTO(CorBloco cor, AndarBloco andar, List<LaminaRequestDTO> laminas) {
+
+    public static BlocoRequestDTO fromEntity(Bloco bloco) {
+        return new BlocoRequestDTO(
+                bloco.getCor(),
+                bloco.getAndar(),
+                bloco.getLaminas().stream()
+                        .map(l -> new LaminaRequestDTO(l.getCor(), l.getPadrao(), l.getPosicaoNoBloco())).toList());
+    }
+
+    public static Bloco toEntity(BlocoRequestDTO blocoDTO) {
+        return Bloco.builder()
+                .cor(blocoDTO.cor)
+                .andar(blocoDTO.andar())
+                .laminas(blocoDTO.laminas.stream().map(LaminaRequestDTO::toEntity).toList())
+                .build();
+    }
+}
--- app_smart_4.0/src/main/java/com/tecdes/smart/app_smart_40/dto/response/LaminaResponseDTO.java
+++ app_smart_4.0/src/main/java/com/tecdes/smart/app_smart_40/dto/response/LaminaResponseDTO.java
@@ -1,23 +1,24 @@
-package com.tecdes.smart.app_smart_40.dto;
+package com.tecdes.smart.app_smart_40.dto.response;
 
+import com.tecdes.smart.app_smart_40.model.Lamina;
 import com.tecdes.smart.app_smart_40.model.enums.CorLamina;
 import com.tecdes.smart.app_smart_40.model.enums.PadraoLamina;
 import com.tecdes.smart.app_smart_40.model.enums.PosicaoLamina;
 
-public record LaminaDTO(
+public record LaminaResponseDTO(
         CorLamina cor,
         PadraoLamina padrao,
         PosicaoLamina posicaoNoBloco) {
 
-    public static LaminaDTO fromEntity(com.tecdes.smart.app_smart_40.model.Lamina lamina) {
-        return new LaminaDTO(
+    public static LaminaResponseDTO fromEntity(Lamina lamina) {
+        return new LaminaResponseDTO(
                 lamina.getCor(),
                 lamina.getPadrao(),
                 lamina.getPosicaoNoBloco());
     }
 
-    public com.tecdes.smart.app_smart_40.model.Lamina toEntity() {
-        return com.tecdes.smart.app_smart_40.model.Lamina.builder()
+    public Lamina toEntity() {
+        return Lamina.builder()
                 .cor(this.cor)
                 .padrao(this.padrao)
                 .posicaoNoBloco(this.posicaoNoBloco)
