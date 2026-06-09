89183e02b9611e1f316b366c3c2f6420ace6cc96
William-Colasso <william.colasso@gmail.com>
2026-06-09 19:32:47 -0300

    add: adicionando classes de erro

---
+++ app_smart_4.0/src/main/java/com/tecdes/smart/app_smart_40/controller/GlobalExceptionHandler.java
@@ -0,0 +1,10 @@
+package com.tecdes.smart.app_smart_40.controller;
+
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+
+}
