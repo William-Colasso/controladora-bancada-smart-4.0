ebaefa86fe2d78c2808170a3bea20b3977ec5485
Leonardo Da SIlva <leonardo_silva48@estudante.sesisenai.org.br>
2026-06-10 19:00:53 -0300

    Import Order
    HandleIllegalArgument tbm captura IllegalArgumentException e comoEstoqueInsuficienteException é uma classe filha pode ser capturado errado, para garantir prioridade adcionei @order

--- app_smart_4.0/src/main/java/com/tecdes/smart/app_smart_40/controller/GlobalExceptionHandler.java
+++ app_smart_4.0/src/main/java/com/tecdes/smart/app_smart_40/controller/GlobalExceptionHandler.java
@@ -10,6 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
+import org.springframework.core.annotation.Order;
 
 import java.time.Instant;
 
