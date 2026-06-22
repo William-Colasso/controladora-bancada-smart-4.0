d25df14fe3d6c18ddd1efaa77cfd537db03c0125
William-Colasso <william.colasso@gmail.com>
2026-06-22 19:56:28 -0300

    add: adicionando models das estações

---
+++ app_smart_4.0/src/main/java/com/tecdes/smart/app_smart_40/model/clp/ExpedicaoCLP.java
@@ -0,0 +1,27 @@
+package com.tecdes.smart.app_smart_40.model.clp;
+
+import org.springframework.stereotype.Component;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Component
+public class ExpedicaoCLP {
+
+    boolean recebidoExpedicao;
+    boolean iniciarGuardarExp;
+    int posicaoGuardarExp;
+
+    int[] orderExpedicao;
+
+    boolean pedirPosicaoExp;
+    int posicaoGuardadoExpedicao;
+    int posicaoRemovidoExpedicao;
+    boolean adicionarExpedicao;
+    boolean removerExpedicao;
+    int opGuardadoExpedicao;
+}
